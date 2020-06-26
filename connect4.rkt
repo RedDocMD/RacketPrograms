0005e2bac00a70a216d62076ca8033f5528be116
Deep Majumder <deep.majumder2019@gmail.com>
2020-06-26 09:50:35 +0530

    Added a static evaluator

--- connect4.rkt
+++ connect4.rkt
@@ -1,20 +1,26 @@
 #lang racket/gui
 (require pict)
 
+
 ;; Dimensions of the Connect4 boards
 (define ROWS 6)
 (define COLUMNS 7)
+(define CHAIN 4)
+
 
 ;; Player info
 (define NO-PLAYER 0)
 (define MAX-PLAYER 1)
 (define MIN-PLAYER 2)
 
+
 ;; Game position struct
 (struct node (board player [alpha #:mutable] [beta #:mutable]) #:transparent)
 
+
 ;; Game state struct
 (struct state ([curr-node #:mutable] [move# #:mutable]))
+
 
 ;; Returns the starting node of the game
 (define (start-node)
@@ -23,9 +29,11 @@
         -inf.0
         +inf.0))
 
+
 ;; Returns the start state of the game
 (define (start-state)
   (state (start-node) 0))
+
 
 ;; Params for displaying board
 (define DIA 50)
@@ -34,6 +42,7 @@
 (define MIN-PLAYER-CIRCLE (disk DIA #:color "Blue"))
 (define HSPACE 2)
 (define VSPACE 2)
+
 
 ;; Generates a pict for the node
 (define (pict-node node)
@@ -46,13 +55,17 @@
                                                              (cond [(equal? tile NO-PLAYER) EMPTY-CIRCLE]
                                                                    [(equal? tile MAX-PLAYER) MAX-PLAYER-CIRCLE]
                                                                    [(equal? tile MIN-PLAYER) MIN-PLAYER-CIRCLE]))))))))))
+
+
 ;; Returns the said column as a list
 (define (get-column tiles column)
   (build-list ROWS (lambda (row) (list-ref tiles (+ (* row COLUMNS) column)))))
 
+
 ;; Returns the said row as a list
 (define (get-row tiles row)
   (build-list COLUMNS (lambda (column) (list-ref tiles (+ (* row COLUMNS) column)))))
+
 
 ;; Returns the number of chains of val of length len in tiles 
 (define (chain-count val len tiles)
@@ -60,6 +73,37 @@
     (count identity (map (lambda (pos)
                            (equal? (count (lambda (num) (equal? num val)) (take (list-tail tiles pos) len)) len))
                          (stream->list (in-range start-lim))))))
+
+
+;; List of coordinates for chain starting at (i, j) and in direction dir
+(define (list-coords-in-dir i j dir)
+  (define (rec-list i j dir)
+    (let ([x (car dir)]
+          [y (cdr dir)])
+      (if (or (< i 0) (< j 0) (>= i ROWS) (>= j COLUMNS))
+          empty
+          (append (list (cons i j)) (rec-list (+ i x) (+ j y) dir)))))
+  (rec-list i j dir))
+
+
+;; Get chains in some direction
+(define (get-chains i j dir tiles)
+  (let ([coords (list-coords-in-dir i j dir)])
+    (map (lambda (coord)
+           (let* ([i (car coord)]
+                  [j (cdr coord)]
+                  [idx (+ (* i COLUMNS) j)])
+             (list-ref tiles idx))))))
+
+
+;; Get chains in the right diagonal direction
+(define (get-right-diagonal-chains i j tiles)
+  (get-chains i j (cons -1 +1) tiles))
+
+
+;; Get chains in the left diagonal direction
+(define (get-left-diagonal-chains i j tiles)
+  (get-chains i j (cons +1 -1) tiles))  
   
 
 ;; Makes a move and returns the next node or #f if illegal move
@@ -81,6 +125,46 @@
               (node-alpha curr-node)
               (node-beta curr-node)))))
 
+;; Returns all consecutive chains of tiles
+(define (get-all-chains tiles)
+  (let* ([row-ids (stream->list (in-range ROWS))]
+         [column-ids (stream->list (in-range COLUMNS))]
+         [position-ids (for*/list ([i row-ids]
+                                   [j column-ids])
+                         (cons i j))])
+    (let ([rows (map (lambda (row) (get-row tiles row)) row-ids)]
+          [columns (map (lambda (column) (get-column tiles column)) column-ids)]
+          [right-diagonals (map (lambda (position) (get-right-diagonal-chains (car position) (cdr position))) position-ids)]
+          [left-diagonals (map (lambda (position) (get-left-diagonal-chains (car position) (cdr position))) position-ids)])
+      (list rows columns right-diagonals left-diagonals))))
+
+
+;; Look for chains of length CHAIN
+(define (find-win-chain chains player)
+  (ormap (lambda (tile-list)
+           (ormap (lambda (chain) (> (chain-count player CHAIN chain) 0)) tile-list))
+         chains))
+
+
+;; Returns no. of chains of length len
+(define (tot-chain-count player len chains)
+  (apply + (map (lambda (tile-list)
+                  (apply + (map (lambda (chain) (chain-count player len chain)) tile-list)))
+                chains)))
+
+
+;; Static evaluator for a node
+(define (static-evaluator curr-node)
+  (let ([tiles (node-board curr-node)]
+        [player (node-player curr-node)])
+    (let* ([chains (get-all-chains tiles)]
+           [value (cond [(find-win-chain chains player) 10000]
+                        [(> (tot-chain-count player 3 chains) 0) 5000]
+                        [(> (tot-chain-count player 2 chains) 0) 1000]
+                        [(> (tot-chain-count player 1 chains) 0) 100])])
+      (if (equal? player MAX-PLAYER)
+          value
+          (- value)))))
 
 
 ;; The mutable state of the game
